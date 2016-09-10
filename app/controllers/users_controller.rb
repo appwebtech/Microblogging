012bbb868a0e8d7f71b500729dd0114a9b9bb845
appwebtech <josembi@gmail.com>
2016-09-10 19:43:56 +0200

    Finish User Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
   	@user= User.new(user_params)
   	if @user.save
-  		#whatever
+      flash[:success] = "Welcome to the Microblogging Website!"
+  		redirect_to @user
   	else
   		render 'new'
   end
