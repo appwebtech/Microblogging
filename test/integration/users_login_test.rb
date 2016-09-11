f5a1e211c452d27e2513c0e0dde4e130acf82b95
appwebtech <josembi@gmail.com>
2016-09-11 12:30:42 +0200

    Enhance Secure Password Encryption

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,5 +1,9 @@
 require 'test_helper'
 
+def setup
+	@user = users(:joseph)
+end
+
 class UsersLoginTest < ActionDispatch::IntegrationTest
 	test "login with invalid information" do 
 		get sessions_new_path
@@ -9,5 +13,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 		assert_not flash.empty?
 		get root_path
 		assert flash.empty?
+	end
+
+	test "login with valid information" do 
+		get sessions_new_path
+		post sessions_create_path, session: { email: @user.email, password: "password" }
+		assert_redirected_to @user 
+		follow_redirect!
+		assert_template 'users/show'
+		assert_select "a[href=?]", sessions_new_path, count: 0
+		assert_select "a[href=?]", sessions_destroy_path
+		assert_select "a[href=?]", user_path(@user)
 	end
 end
