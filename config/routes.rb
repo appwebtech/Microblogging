5b747c8abc015b2712ec44b8b731670f965346de
appwebtech <josembi@gmail.com>
2016-09-11 10:55:44 +0200

    Controller Session & Log in Form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get   'static_pages/help'
-  get   'static_pages/about'
-  get  'static_pages/contact'
-  get 	'users/new'
+  get 'sessions/new'
+
+  root 		'static_pages#home'
+  get   	'static_pages/help'
+  get   	'static_pages/about'
+  get  		'static_pages/contact'
+  get 		'users/new'
+  get 		'sessions/new'
+  post 		'sessions/create'
+  delete 	'sessions/destroy'
   resources :users
 end
