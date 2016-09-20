cb53c06ea6f9c26ca370e477b10a322c072dff4a
appwebtech <josembi@gmail.com>
2016-09-20 21:43:57 +0200

    Reset and Re-seed DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,9 @@ User.create!(name: "Joseph",
 						 email: "joseph@example.com",
 						 password:  						"foobar",
 						 password_confirmation: "foobar",
-						 admin: true)
+						 admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
@@ -13,5 +15,7 @@ User.create!(name: "Joseph",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
