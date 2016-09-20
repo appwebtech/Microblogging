f90c391f0eda5e715625a6d09949917db77cf8f8
appwebtech <josembi@gmail.com>
2016-09-20 21:28:13 +0200

    Generate Controller & Fix Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   get 'sessions/new'
 
   root 		'static_pages#home'
@@ -10,4 +11,5 @@ Rails.application.routes.draw do
   post 		'sessions/create'
   delete 	'sessions/destroy'
   resources :users
+  resources :account_activations, only: [:edit]
 end
