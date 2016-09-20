f7800213ec71241b6db5aa5e174d02f61cbf97ff
appwebtech <josembi@gmail.com>
2016-09-20 23:41:05 +0200

    Last commit for today †† NOT ON MASTER BRANCH ††

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,7 +1,10 @@
 class AccountActivationsController < ApplicationController
 
 	def edit
-		
+		user = User.find_by(email: params[:email])
+		if user && user.authenticated?(:activation, params[:id])
+		else
+			#Handle invalid link.
+		end
 	end
- 
 end
